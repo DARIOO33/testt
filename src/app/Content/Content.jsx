a1de55371d852100038a089c345e6648009d1fcc
DARIOO33 <darioanwar3@gmail.com>
2023-07-15 22:06:39 +0100

    nthng

--- src/app/Content/Content.jsx
+++ src/app/Content/Content.jsx
@@ -2,13 +2,12 @@ import './Content.css'
 import Dash from '../Dash/Dash.jsx'
 import Blogs from '../Blogs/Blogs.js'
 export default function Content ({arr}) {
-    const array = {arr};
     // console.log({array});
     return (
         <div>
             <div className="content">
                 <Dash  />
-                <Blogs ppp='6' blogs = {array}/>
+                <Blogs ppp='6' blogs = {arr}/>
             </div>
         </div>
     )
